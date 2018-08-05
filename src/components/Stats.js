8a713aa3a61edb19e8a25132018f38f158bf4980
ranasepulsa <rana@sepulsa.com>
2018-08-05 21:42:39 +0700

    stats  page

---
+++ src/components/Stats.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Header from "./Header";
+import Chart from "./chart/Chart";
+
+class List extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="stats-container">
+          <Chart />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default List;
