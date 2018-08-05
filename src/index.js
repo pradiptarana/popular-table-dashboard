a920efd783408fac8b6feec45ae7aeadbca93b5c
ranasepulsa <rana@sepulsa.com>
2018-08-05 23:26:06 +0700

    add index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+import Routes from "./routes";
+import "./App.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Sync mobx storeswith browser history
+import { Provider } from "mobx-react";
+import browserHistory from "react-router/lib/browserHistory";
+import { RouterStore, syncHistoryWithStore } from "mobx-react-router";
+// import './assets/css/style.css'
+
+// Create routing store
+const routingStore = new RouterStore();
+
+// Assign routingStore to store object
+Object.assign({
+  routing: routingStore
+});
+
+// Sync browser history with stores
+const history = syncHistoryWithStore(browserHistory, routingStore);
+
+// Render Routing and wrap it with mobx provider
+
+ReactDOM.render(
+  <Provider>
+    <Routes history={history} />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
